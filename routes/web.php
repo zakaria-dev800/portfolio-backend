569b10ea650ea85df7ede1fb078cb7690b1e5baf
Zakaria <elkorchizakariya@example.com>
2026-03-15 03:37:31 +0000

    import dist frontened

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('frontend/index.html'));
+})->where('any', '.*');
